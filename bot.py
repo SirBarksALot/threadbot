95b8ef65ad064484a8956c9e1ab4d620f975e05b
filip.lezuch <filiplez18@gmail.com>
2019-04-11 10:29:23 +0200

    PEP8 adjusted

--- bot.py
+++ bot.py
@@ -1,107 +1,98 @@
 import os
 import win32gui
 import time
-import PIL
 import threading
 import random
-from pynput.keyboard import Key, Controller
-
+from pynput.keyboard import Controller
 
 keyboard = Controller()
 lock = threading.Lock()
 
-global handlers
+
 handlers = []
 
 
-def openFile():
+def open_file():
     try:
         os.startfile('C:\\WINDOWS\\system32\\notepad.exe')
         print("Starting program...")
     except Exception as e:
         print(str(e))
 
-def selectingActiveWindow(activewindowhandler):           
-    win32gui.SetForegroundWindow(activewindowhandler)
+
+def select_active_window(active_window_handler):
+    win32gui.SetForegroundWindow(active_window_handler)
+
 
 def typing(role):
     if role == 'Main':
-        selectingActiveWindow(handlers[0])
+        select_active_window(handlers[0])
         keyboard.press('A')
         keyboard.release('A')
         print('Main act')
-        
-    elif role == 'sup':
-        selectingActiveWindow(handlers[1])
+
+    elif role == 'Sup':
+        select_active_window(handlers[1])
         keyboard.press('W')
         keyboard.release('W')
         print('Sup act')
 
     else:
-        print ('Role not slected!')
+        print('Role not selected!')
+
 
 def window_setup():
     time.sleep(3)
-        
-    def enumHandler(hwnd, handlers):
+
+    def enum_handler(hwnd, handle):
         if win32gui.IsWindowVisible(hwnd):
             if 'Notepad' in win32gui.GetWindowText(hwnd):
-                handlers.append (hwnd)
-                    
-                if len(handlers) > 1:
-                        win32gui.MoveWindow(handlers[0], 0, 0, 699, 1030, True)
-                        win32gui.MoveWindow(handlers[1], 700, 0, 699, 1030, True)
-                        #win32gui.MoveWindow(handlers[0], 0, 0, 1600, 1030, True)
-                        #win32gui.MoveWindow(handlers[1], 0, 0, 1600, 1030, True)
+                handle.append(hwnd)
+                if len(handle) > 1:
+                    win32gui.MoveWindow(handle[0], 0, 0, 499, 1030, True)
+                    win32gui.MoveWindow(handle[1], 500, 0, 499, 1030, True)
 
             if 'Python' in win32gui.GetWindowText(hwnd):
-                    win32gui.MoveWindow(hwnd, 1400, 0, 520, 1030, True)
+                win32gui.MoveWindow(hwnd, 1400, 0, 520, 1030, True)
+
+    win32gui.EnumWindows(enum_handler, handlers)
 
 
-    win32gui.EnumWindows(enumHandler, handlers)
-
-
-                    
 def setup():
-    for j in range (1,3):
-        openFile()
+    for j in range(1, 3):
+        open_file()
     window_setup()
+
 
 setup()
 
 
-def mainThread():
+def main_thread():
     while True:
         with lock:
             typing('Main')
-        time.sleep(random.uniform(3.0,5.0))
-    
-def supportThread():
+        time.sleep(random.uniform(1.0, 3.0))
+
+
+def support_thread():
     while True:
         with lock:
             typing('Sup')
-        time.sleep(random.uniform(1080.0,1100.0))
+        time.sleep(random.uniform(2.0, 3.0))
 
-def threader(threadNumber, name):
-    print(threadNumber)
-    if threadNumber == 1:
-        mainThread()
-    elif threadNumber == 2:
-        supportThread()
+
+def threader(thread_number, name):
+    print(thread_number, name)
+    if thread_number == 1:
+        main_thread()
+    elif thread_number == 2:
+        support_thread()
     else:
-        print('threadNumber oolist?')
+        print('thread_number out of list?')
 
 
-t1 = threading.Thread(target = threader, name = 'Mainthread', args = (1, 'Mainthread'))
+t1 = threading.Thread(target=threader, name='Mainthread', args=(1, 'Mainthread'))
 t1.start()
 
-t2 = threading.Thread(target = threader, name = 'Supthread', args = (2, 'Supthread'))
+t2 = threading.Thread(target=threader, name='Supthread', args=(2, 'Supthread'))
 t2.start()
-
-
-
-    
-
-
-
-
